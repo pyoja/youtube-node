dec23065fa0de2416cdc6a4a73f1c92f9cb3512c
pyoja <wjdgns13nr@naver.com>
2023-01-28 22:52:49 +0900

    로그인json

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,41 @@
 "use strict";
 
-const home = (req, res) => {
-  res.render("home/index");
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    res.render("home/login");
+  },
 };
 
-const login = (req, res) => {
-  res.render("home/login");
+const users = {
+  id: ["aaa", "bbb"],
+  pw: ["aaa", "bbb"],
+};
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id;
+    const pw = req.body.pw;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.pw[idx] === pw) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: "로그인 실패",
+    });
+  },
 };
 
 module.exports = {
-  home,
-  login,
+  output,
+  process,
 };
