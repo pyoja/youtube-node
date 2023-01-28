d983f05f47e1cf5c2721ca5ddb14e70947be8f55
pyoja <wjdgns13nr@naver.com>
2023-01-28 20:24:35 +0900

    DOM을 이용하여 아디비번 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log("hello");
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+  console.log(req);
+}
