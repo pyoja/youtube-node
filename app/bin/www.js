3d34137c8d8686e814aff8d59890e2b6fad95a2f
pyoja <wjdgns13nr@naver.com>
2023-01-30 23:36:00 +0900

    AWS연동 및 dotenv 환경변수 등록

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const app = require("../app");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("서버 가동");
--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,15 +9,19 @@ class User {
 
   async login() {
     const client = this.body;
-    const { id, pw } = await UserStorage.getUserInfo(client.id);
+    try {
+      const { id, pw } = await UserStorage.getUserInfo(client.id);
 
-    if (id) {
-      if (id === client.id && pw === client.pw) {
-        return { success: true };
+      if (id) {
+        if (id === client.id && pw === client.pw) {
+          return { success: true };
+        }
+        return { success: false, msg: "비밀번호 틀림" };
       }
-      return { success: false, msg: "비밀번호 틀림" };
+      return { success: false, msg: "존재하지 않는 아이디" };
+    } catch (err) {
+      return { success: false, msg: err };
     }
-    return { success: false, msg: "존재하지 않는 아이디" };
   }
 
   async register() {
