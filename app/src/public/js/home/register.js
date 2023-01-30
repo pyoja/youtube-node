82bbbe45638c317ca18a879fd950c4024b4e2991
pyoja <wjdgns13nr@naver.com>
2023-01-30 13:38:11 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,20 +1,22 @@
 "use strict";
 
 const id = document.querySelector("#id");
+const name = document.querySelector("#name");
 const pw = document.querySelector("#pw");
-const loginBtn = document.querySelector("#button");
+const confirmPw = document.querySelector("#confirm-pw");
+const registerBtn = document.querySelector("#button");
 
-loginBtn.addEventListener("click", login);
+registerBtn.addEventListener("click", register);
 
-function login() {
+function register() {
   const req = {
     id: id.value,
+    name: name.value,
     pw: pw.value,
+    confirmPw: confirmPw.value,
   };
-  console.log(req);
-  console.log(JSON.stringify(req));
 
-  fetch("/login", {
+  fetch("/register", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -24,12 +26,12 @@ function login() {
     .then((res) => res.json())
     .then((res) => {
       if (res.success) {
-        location.href = "/";
+        location.href = "/login";
       } else {
         alert(res.msg);
       }
     })
     .catch((err) => {
-      console.error(new Error("로그인 중 에러 발생"));
+      console.error(new Error("회원가입 중 에러 발생"));
     });
 }
